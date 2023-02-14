41b0c46468c57d0919ed73a0352c733e9a690ce0
hambergjesse <hambergjesse@gmail.com>
2023-02-14 10:23:26 +0200

    header + homePage start

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,14 @@
+/* eslint-disable react/prop-types */
+
+// import context
 import { useContext } from "react";
 import { LanguageContext } from "../../langLocal/context/langContext";
-import { BsFacebook } from "react-icons/bs";
+
+// import icons from react-icons
+import { BsFacebook, BsLinkedin, BsInstagram } from "react-icons/bs";
+
+// import virittämö helsinki logo
+import VirittamoLogo from "./assets/virittamo-helsinki.png";
 
 const Header = () => {
   const { lang, setLocale, fi } = useContext(LanguageContext);
@@ -10,47 +18,74 @@ const Header = () => {
   return (
     <main className="header__wrapper">
       <section className="header__container">
+        <img src={VirittamoLogo} alt="Virittämö Helsinki" />
+        {/* nav links */}
         <nav className="header__nav">
           <ul className="header__nav--list">
-            <li className="header__nav--list-item">{header.homepage}</li>
-            <li className="header__nav--list-item">{header.projects}</li>
-            <li className="header__nav--list-item">{header.experiences}</li>
-            <li className="header__nav--list-item">{header.contact}</li>
+            <li className="header__nav--list-item">
+              <a href="/">{header.home_page}</a>
+            </li>
+            <li className="header__nav--list-item">
+              <a href="/projects">{header.projects}</a>
+            </li>
+            <li className="header__nav--list-item">
+              <a href="/experiences">{header.experiences}</a>
+            </li>
+            <li className="header__nav--list-item">
+              <a href="/contact">{header.contact}</a>
+            </li>
           </ul>
         </nav>
+        {/* virittämö social icons */}
         <div className="header__socials">
-          <a href="/">
-            <BsFacebook />
+          <a
+            href="https://www.facebook.com/virittamohelsinki/"
+            alt="Virittämö Facebook Link"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <BsFacebook className="social-icon" />
           </a>
-          <a href="/">
-            <BsFacebook />
+          <a
+            href="https://www.linkedin.com/company/virittamohelsinki/"
+            alt="Virittämö Linkedin Link"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <BsLinkedin className="social-icon" />
           </a>
-          <a href="/">
-            <BsFacebook />
+          <a
+            href="https://www.instagram.com/virittamohelsinki/"
+            alt="Virittämö Instagram Link"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <BsInstagram className="social-icon" />
           </a>
-        </div>
-        <div className="header__language">
-          {lang === fi ? (
-            <a
-              href="/"
-              onClick={() => {
-                setLocale("en");
-                localStorage.setItem("virittamo-lang", "en");
-              }}
-            >
-              EN
-            </a>
-          ) : (
-            <a
-              href="/"
-              onClick={() => {
-                setLocale("fi");
-                localStorage.setItem("virittamo-lang", "fi");
-              }}
-            >
-              FI
-            </a>
-          )}
+          {/* fi/en language switch button */}
+          <div className="header__language">
+            {lang === fi ? (
+              <a
+                href="#"
+                onClick={() => {
+                  setLocale("en");
+                  localStorage.setItem("virittamo-lang", "en");
+                }}
+              >
+                EN
+              </a>
+            ) : (
+              <a
+                href="#"
+                onClick={() => {
+                  setLocale("fi");
+                  localStorage.setItem("virittamo-lang", "fi");
+                }}
+              >
+                FI
+              </a>
+            )}
+          </div>
         </div>
       </section>
     </main>
--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,21 +1,23 @@
-/*
-import { LanguageContext } from "../../langLocal/context/langContext";
+// import context
 import { useContext } from "react";
-*/
+import { LanguageContext } from "../../langLocal/context/langContext";
+
+// import assets
+import HeaderImg from "./assets/main-header-img.jpeg";
 
 // import components
 import Header from "../../components/Header/Header";
 
 const HomePage = () => {
-  /*
-  const {
-    lang: { home_page },
-  } = useContext(LanguageContext);
-  */
+  const { lang } = useContext(LanguageContext);
+
+  const { home_page } = lang;
 
   return (
     <main className="homePage__wrapper">
       <Header />
+      <img src={HeaderImg} alt="Home page main image" />
+      <h1>{home_page.home_title}</h1>
     </main>
   );
 };
