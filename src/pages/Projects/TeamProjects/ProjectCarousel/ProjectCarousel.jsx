703c5cd28ffc452b22f26806b7503358a9f15fc1
Jesse Hamberg <hambergjesse@gmail.com>
2023-03-21 18:42:11 +0200

    projectPage carousel responsiveness

--- src/pages/Projects/TeamProjects/ProjectCarousel/ProjectCarousel.jsx
+++ src/pages/Projects/TeamProjects/ProjectCarousel/ProjectCarousel.jsx
@@ -7,27 +7,33 @@ import { BsArrowLeftCircle, BsArrowRightCircle } from "react-icons/bs";
 
 import { LoadingSlides } from "../../../Home/Carousel/SubComponents/LoadingSlides";
 
+const getNumVisibleSlides = (slides) => {
+  const width = window.innerWidth;
+  if (width <= 1080) {
+    return 1;
+  } else if (width <= 1440) {
+    return 2;
+  } else {
+    return Math.min(slides.length, 3);
+  }
+};
+
 export const ProjectCarousel = ({ text, slides }) => {
   const [activeIndex, setActiveIndex] = useState(0);
+  const numVisibleSlides = getNumVisibleSlides(slides);
 
   const handlePrevClick = () => {
-    if (activeIndex > 0) {
-      setActiveIndex(activeIndex - 1);
-    }
+    setActiveIndex(activeIndex - 1);
   };
 
   const handleNextClick = () => {
-    if (activeIndex < slides.length - 3) {
-      setActiveIndex(activeIndex + 1);
-    }
+    setActiveIndex(activeIndex + 1);
   };
 
   const renderActiveItems = () => {
-    let activeItems = [
-      slides[activeIndex],
-      slides[activeIndex + 1],
-      slides[activeIndex + 2],
-    ].filter(Boolean);
+    let activeItems = slides
+      .slice(activeIndex, activeIndex + numVisibleSlides)
+      .filter(Boolean);
 
     return activeItems.map((activeItem, index) => {
       const ActiveItem = activeItem.component;
@@ -36,16 +42,20 @@ export const ProjectCarousel = ({ text, slides }) => {
   };
 
   useEffect(() => {
-    if (activeIndex === -1 || activeIndex + 2 >= slides.length) {
+    if (activeIndex < 0) {
       setActiveIndex(0);
+    } else if (activeIndex + numVisibleSlides > slides.length) {
+      setActiveIndex(slides.length - numVisibleSlides);
     }
   }, [activeIndex, slides]);
 
+  const { title, description, contact } = text;
+
   return (
     <div className="projectPage__teams--container-item">
-      <h2>{text.title}</h2>
-      <p>{text.description}</p>
-      <p>{text.contact}</p>
+      <h2>{title}</h2>
+      <p>{description}</p>
+      <p>{contact}</p>
       <div className="projectPage__teams--carousel">
         <button
           className="arrow-button"
@@ -60,7 +70,7 @@ export const ProjectCarousel = ({ text, slides }) => {
         <button
           className="arrow-button"
           onClick={handleNextClick}
-          disabled={activeIndex >= slides.length - 3}
+          disabled={activeIndex >= slides.length - numVisibleSlides}
         >
           <BsArrowRightCircle className="arrow-button--icon" />
         </button>
