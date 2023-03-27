8ca6cb0abcad5ae682f02a0ae450ae69e06db169
hambergjesse <hambergjesse@gmail.com>
2023-03-27 13:12:47 +0300

    improve instagram iframe for homePage carousel

--- src/pages/Home/Carousel/SubComponents/Instagram.jsx
+++ src/pages/Home/Carousel/SubComponents/Instagram.jsx
@@ -1,6 +1,37 @@
 import { motion } from "framer-motion";
+import { useEffect } from "react";
 
 const Instagram = () => {
+  const setCookieAttributes = () => {
+    document.cookie = "mid=value; SameSite=None; Secure";
+    document.cookie = "ig_did=value; SameSite=None; Secure";
+    document.cookie = "csrftoken=value; SameSite=None; Secure";
+  };
+
+  useEffect(() => {
+    const handleLoad = () => {
+      if (navigator.userAgentData) {
+        const brands = navigator.userAgentData.brands;
+        if (brands.some((brand) => brand.brand === "Google Chrome")) {
+          setCookieAttributes();
+        }
+      } else {
+        if (isChrome()) {
+          setCookieAttributes();
+        }
+      }
+    };
+
+    window.addEventListener("load", handleLoad);
+    return () => window.removeEventListener("load", handleLoad);
+  }, []);
+
+  const isChrome = () => {
+    return (
+      !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime)
+    );
+  };
+
   return (
     <motion.iframe
       className="carousel__item--instagram"
