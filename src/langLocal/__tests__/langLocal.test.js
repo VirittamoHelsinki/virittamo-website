316e44531f9e7a8b4de160c47004eee47391c751
hambergjesse <hambergjesse@gmail.com>
2023-04-13 14:23:42 +0300

    langLocal.test.js wording

--- src/langLocal/__tests__/langLocal.test.js
+++ src/langLocal/__tests__/langLocal.test.js
@@ -2,7 +2,7 @@ import { en } from "../en";
 import { fi } from "../fi";
 
 // checks that the language object keys are identical
-test("two objects have the same keys", () => {
+test("language objects / translations have the same keys", () => {
   expect(en).toBeDefined();
   expect(fi).toBeDefined();
   expect(Object.keys(en).sort()).toEqual(Object.keys(fi).sort());
