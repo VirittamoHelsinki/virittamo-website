364e6a8b1a03c6c8feaff3dcdfaaef84956e69e5
hambergjesse <hambergjesse@gmail.com>
2023-05-17 15:19:41 +0300

    established data flow with strapi and react

--- client/src/pages/Projects/Projects.jsx
+++ client/src/pages/Projects/Projects.jsx
@@ -1,6 +1,8 @@
 // Import the language context and useContext hook from React
 import { LanguageContext } from "../../langLocal/context/langContext";
-import { useContext, Suspense } from "react";
+import { useContext, Suspense, useEffect } from "react";
+
+import axios from "axios";
 
 // Import the Loading component
 import Loading from "../Loading/Loading";
@@ -19,6 +21,28 @@ const Projects = () => {
 
   // Extract the title and description from the language data
   const { title, desc } = lang.projects_page;
+
+  const token = import.meta.env.VITE_STRAPI_API_TOKEN;
+
+  const pullData = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:1337/api/headers?populate=*",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log("Data: ", response.data);
+    } catch (error) {
+      console.log("An error occurred:", error.response);
+    }
+  };
+
+  useEffect(() => {
+    pullData();
+  });
 
   return (
     <Suspense fallback={<Loading />}>
