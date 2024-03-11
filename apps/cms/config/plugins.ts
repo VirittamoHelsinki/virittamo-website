6e1e28bd90ef2667d30529be58aa3c6588885073
kamm3r <marcokammer59@gmail.com>
2024-03-11 10:53:41 +0200

    cms/docker and package.json fix

--- apps/cms/config/plugins.ts
+++ apps/cms/config/plugins.ts
@@ -3,6 +3,7 @@ export default ({ env }) => ({
     config: {
       provider: "strapi-provider-upload-azure-storage",
       providerOptions: {
+        authType: env("STORAGE_AUTH_TYPE", "default"),
         account: env("STORAGE_ACCOUNT"),
         accountKey: env("STORAGE_ACCOUNT_KEY"), //either account key or sas token is enough to make authentication
         containerName: env("STORAGE_CONTAINER_NAME"),
